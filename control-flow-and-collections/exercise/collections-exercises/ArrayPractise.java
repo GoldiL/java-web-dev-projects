22e2d4ce8eca06fc9effb5e7e2906c451dd3557d
GoldiL <goldialbina@gmail.com>
2023-08-15 18:22:35 -0500

    ArrayPractise

---
+++ control-flow-and-collections/exercise/collections-exercises/ArrayPractise.java
@@ -0,0 +1,14 @@
+public class ArrayPractise {
+
+    public static void main(String[] args) {
+        int[] integerArray ={1, 1, 2, 3, 5, 8};
+
+        for(int i =0;i< integerArray.length;i++){
+            if(i%2 ==1){
+                System.out.println(integerArray[i]);
+            };
+
+        }
+    }
+
+}
