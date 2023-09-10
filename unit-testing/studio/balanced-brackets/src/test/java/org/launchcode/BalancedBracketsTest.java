199b5f4cfdb18d474e68f011d5717d9568475e8a
GoldiL <goldialbina@gmail.com>
2023-09-09 21:48:15 -0500

    BalancedBrackets updates

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -1,13 +1,36 @@
 package org.launchcode;
 
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BalancedBracketsTest {
     //TODO: add tests here
+
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void testSigngleSet() {
+
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+
+    }
+
+    @Test
+    public void testDoubleSet() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]]"));
+    }
+
+    public void testTriSet() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]][]"));
+    }
+    public void testMultiSet() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Launch[]Code]][]"));
+    }
+
+    @Test
+    public void testOpenWithCloseFails() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]["));
     }
 }
