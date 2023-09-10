9584398bbdba7d1835e0539f80ed52a133b547dc
GoldiL <goldialbina@gmail.com>
2023-09-09 21:56:27 -0500

    updating test for balance bracket

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -22,15 +22,33 @@ class BalancedBracketsTest {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]]"));
     }
 
+    @Test
     public void testTriSet() {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]][]"));
     }
+    @Test
     public void testMultiSet() {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[[Launch[]Code]][]"));
+    }
+    @Test
+    public void bracketThatDontMatch() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch"));
+    }
+    @Test
+    public void testMulSet() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[]Code"));
+    }
+    @Test
+    public void testtiSet() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[]Code]][]"));
+    }
+    @Test
+    public void tetiSet() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[Launch]Code]]"));
     }
 
     @Test
     public void testOpenWithCloseFails() {
-        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode["));
     }
 }
